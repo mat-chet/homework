98b604983e52df60f8a36f5c9820a67991d3afad
mat-chet <60486391+mat-chet@users.noreply.github.com>
2020-02-19 20:48:10 +0300

    Update and rename Calc.java to CalcWithCounter.java

--- h4/com/company/CalcWithCounter.java
+++ h4/com/company/CalcWithCounter.java
@@ -1,31 +1,45 @@
 package com.company;
 
-public class Calc implements ICalc {
-
+public class CalcWithCounter implements ICalc {
+    private int counter;
     private ICalc iCalc;
 
-    public Calc(ICalc iCalc){
+    public CalcWithCounter(ICalc iCalc){
+        counter = 0;
         this.iCalc = iCalc;
     }
 
     public double sum(double a,double b){
+
+        counter++;
         return iCalc.sum(a,b);
     }
     public double razn(double a, double b){
+
+        counter++;
         return iCalc.razn(a, b);
     }
 
     public double pr(double a, double b){
+        counter++;
+
         return iCalc.pr(a, b);
     }
 
     public double chas(double a, double b){
+
+        counter++;
         return iCalc.chas(a, b);
     }
 
     public double abs(double a){
 
+        counter++;
+
         return iCalc.abs(a);
     }
 
+    public int getCounter() {
+        return counter;
+    }
 }
