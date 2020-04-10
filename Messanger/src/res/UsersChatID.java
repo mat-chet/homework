85fbf99b9bd71ee8756955e369d4ec3c06ca6a12
mat-chet <60486391+mat-chet@users.noreply.github.com>
2020-04-10 22:19:53 +0300

    Create UsersChatID.java

---
+++ Messanger/src/res/UsersChatID.java
@@ -0,0 +1,13 @@
+package res;
+
+import java.util.ArrayList;
+
+public class UsersChatID {
+    public ArrayList<String> usersNik;
+    public int ID;
+
+    public UsersChatID(ArrayList<String> usersNik, int ID) {
+        this.usersNik = usersNik;
+        this.ID = ID;
+    }
+}
